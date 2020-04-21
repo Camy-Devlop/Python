65bd116ea4d480420eab2f5434f5f9a981120ed8
adbaibi <adbaibi.ismail@gmail.com>
2020-04-21 19:18:50 +0200

    ok

---
+++ tp/main.py
@@ -0,0 +1,8 @@
+
+from pkg.tplist import ListMots 
+
+class main:
+
+    l=ListMots()
+    l.demande_liste_mot()
+    print("le tableau "+str(l.melanger_mot_List()))          
---
+++ tp/pkg/tplist.py
@@ -0,0 +1,26 @@
+from random import shuffle
+
+class ListMots:
+
+    list_mot=[]
+    def __init__(self):
+        self.list_mot
+        self.list_mot.clear()
+        
+
+    def demande_liste_mot(self):  
+        self.list_mot= input("viellez entre des mot et chause mot separer par un / ").split("/")
+        print(self.list_mot)
+    
+    def melanger_mot_List(self):
+        print("liste dans la classe "+str(self.list_mot))
+        m=self.list_mot
+        
+        
+        return  m
+
+
+
+       
+    
+    
