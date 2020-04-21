c15092e5539c234d42569bd4221d2fdc935a7535
adbaibi <adbaibi.ismail@gmail.com>
2020-04-21 02:48:10 +0200

    ok

---
+++ compteur_voyelle.py
@@ -0,0 +1,14 @@
+
+class Voyelle:
+
+    voyelle="aeiouy"
+
+    def get_voyelle_numbers(self,mot):
+        print("bonjour "+mot)
+  
+    def recher_voyelle(lettre):
+        lettre=lettre.lower()
+        x=0
+
+v=Voyelle()
+v.get_voyelle_numbers("patrik")
