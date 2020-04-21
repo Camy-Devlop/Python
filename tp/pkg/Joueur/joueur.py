204bf153f5cb49a1d40dfe16010ad5b06a1c3a4b
adbaibi <adbaibi.ismail@gmail.com>
2020-04-22 01:52:33 +0200

    ok

--- tp/pkg/Joueur/joueur.py
+++ tp/pkg/Joueur/joueur.py
@@ -5,7 +5,7 @@ from personne import Personne
 #joueur 
 class Joueur(Personne):
 
-    #initisalisation de la class Joueur les attribue 
+    #initisalisation de la class Joueur les attribue  
         #   nom obligatoire 
         #   attribue optionnelle prenom, date_nessance et sexe 
     def __init__(self,nom,prenom="",date_nessance="",sexe=""):
