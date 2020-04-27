36231bb4da75e77d156e5298a2ef00b6d9d85d2f
adbaibi <adbaibi.ismail@gmail.com>
2020-04-27 23:23:36 +0200

    ok

--- tp3/main.py
+++ tp3/main.py
@@ -10,14 +10,6 @@ class main(object):
     print("bienvenu dans ma ville")
     window =Tk()
     
-    app=list()
-    a=list()
-      
-    for i in range(10):
-        a.clear()
-        for j in range(9):
-            a.append(Appartement(i,str(chr(j+97))))
-              
-        app.append(a)
+    
       
     
