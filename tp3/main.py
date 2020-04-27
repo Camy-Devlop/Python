99ca98770f0c4d7d92f51a7938b1b0cfd7e2f609
adbaibi <adbaibi.ismail@gmail.com>
2020-04-27 22:13:50 +0200

    ok

--- tp3/main.py
+++ tp3/main.py
@@ -1,14 +1,15 @@
 
 #! /usr.bin/python3
-
+from tkinter import *
 
 from Pkg.Batiments.Immeuble import Immeuble
 from Pkg.Batiments.Appartement import Appartement 
 
-class main:
+class main(object):
   
     print("bienvenu dans ma ville")
-     
+    window =Tk()
+    
     app=list()
     a=list()
       
