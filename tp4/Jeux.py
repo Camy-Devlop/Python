d737c034c200623d1ed0f0172b300cec30dbd701
adbaibi <adbaibi.ismail@gmail.com>
2020-05-02 17:09:42 +0200

    petit commentaire dans le fichier jeux.py

--- tp4/Jeux.py
+++ tp4/Jeux.py
@@ -5,6 +5,8 @@ from random import shuffle as molo
 from Module.joueur import Joueur
 from Module.paquet import Un_paquet_cartes
 
+# c'est un vieux jeux UNO que je voudrais realiser 
+#petit defi perso
 
 class main:
     
