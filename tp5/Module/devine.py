c32834af3e295594c083f2d660475837ed2c7ace
adbaibi <adbaibi.ismail@gmail.com>
2020-05-11 04:19:08 +0200

    ok

---
+++ tp5/Module/devine.py
@@ -0,0 +1,20 @@
+
+
+class Devine:
+
+    def __init__(self):
+        self.player="walid"
+    
+    def affiche_nom_joueur(self):
+        print("Bienvenu au jeux "+self.player)
+
+    def multiplication(self,valeur1,valeur2):
+      resultat = valeur1 * valeur2 
+      print(str(valeur1)+" * "+str(valeur2)+" = "+str(resultat)) 
+
+
+
+class Kiki:
+
+    def __init__(self):
+        pass
