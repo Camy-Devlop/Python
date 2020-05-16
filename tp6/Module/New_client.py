cc09e8173d053fd6ae15c23171dea1862efcf163
adbaibi <adbaibi.ismail@gmail.com>
2020-05-16 23:42:51 +0200

    MAJ 16/05/2020

--- tp6/Module/New_client.py
+++ tp6/Module/New_client.py
@@ -1,11 +1,17 @@
+from Module.Adresse import Adresse
+from Module.Personne import Personne
+
 
 class New_Client():
 
     def __init__(self):
         self.nom:str=(input("Nom du client: ")).upper()
         self.prenom:str=(input("Prénom du client: ")).upper()
-        print(self.nom,"\n"+self.prenom)
+        self.adr:Adresse=Adresse(input("Adresse: "),int(input("Numero: ")),int(input("Code postal: ")),input("commune: "),input("Pays: "))
 
+    def get_personne(self)->Personne:
+        if self.nom!=None and self.prenom!=None:
+            return Personne(self.nom,self.prenom,self.adr.__str__())
 
 if __name__ == __name__:
-    New_Client()
+   print( New_Client().get_personne())
--- tp6/Module/Personne.py
+++ tp6/Module/Personne.py
@@ -1,11 +1,12 @@
 from Module.Adresse import Adresse
-
+from datetime import date
 
 class Personne():
-    def __init__(self,nom:str,prenom:str,adresse:Adresse):
+    def __init__(self,nom:str,prenom:str,adresse:Adresse,date_naissance:date,lieux_naissance:str):
         self.nom:str=nom
         self.prenom:str=prenom
         self.adresse:Adresse=adresse
+
 
     def __str__(self):
         return self.nom+" "+self.prenom
