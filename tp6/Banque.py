fc9efc7cf875ff863f90882e4caa48dcfa8485d3
adbaibi <adbaibi.ismail@gmail.com>
2020-05-17 04:54:45 +0200

    MAJ 17/05/2020

--- tp6/Banque.py
+++ tp6/Banque.py
@@ -4,16 +4,21 @@ from Module.Adresse import Adresse
 from Module.Client import Client
 from Module.Compte import Compte
 from Module.String_int import String_int
-
-adr=Adresse("avenue de l'expo. univer.",50,1083,"chichi","bruxelles")
-comp=Compte(String_int("BE",3),2934,9991,2100)
+from datetime import date
+#adr=Adresse("avenue de l'expo. univer.",50,1083,"chichi","bruxelles")
+#comp=Compte(String_int("BE",3),2934,9991,2100)
 
 #===================================================================================
 
 #===================================================================================
 print("============== Client ==============")
 
-client=Client("Adbaibi","Ismail",comp,adr)
+#client=Client("Adbaibi","Ismail",comp,adr)
 
-print(client)
-print(client.get_adress())
+#print(client)
+#print(client.get_adress())
+
+print("============== Date ==============")
+d=date(2020,1,3)
+
+print(d.strftime("%d/%m/%Y"))
--- tp6/Module/Personne.py
+++ tp6/Module/Personne.py
@@ -2,15 +2,42 @@ from Module.Adresse import Adresse
 from datetime import date
 
 class Personne():
+
     def __init__(self,nom:str,prenom:str,adresse:Adresse,date_naissance:date,lieux_naissance:str):
+        """
+            va cree une base pour une personne avec son nom, nom prenom, son adresse, la date de naissance
+            et le lieux de naissance
+        :param nom: le nom de la personne string
+        :param prenom: le prenom de la personne en string
+        :param adresse:  class Adresse permet de stocker l'adresse de la personne
+        :param date_naissance: type Date permet de stoker la date de naissance
+        :param lieux_naissance: lieu de naissance type string
+        """
         self.nom:str=nom
         self.prenom:str=prenom
         self.adresse:Adresse=adresse
         self.date_nee:date=date_naissance
         self.lieux_nee:str=lieux_naissance
+        self.format_date:str="%d/%m/%Y"
 
     def __str__(self):
+        """
+            fonction general qui retourne un string
+        :return: retourne le nom et le prenom de la personne
+        """
         return self.nom+" "+self.prenom
 
     def get_adress(self)->str:
+        """
+        permet de retourner l'adresse de la personne
+        :return: retourne l'adresse de la personne.
+        """
         return self.adresse.__str__()
+
+
+    def get_date_nee(self)->str:
+        return self.date_nee.strftime(self.format_date)
+
+
+    def get_lieux_nee(self)->str:
+        return self.lieux_nee
