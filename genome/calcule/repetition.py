3edfe12dc6d21986d03437f012ed6555b910506a
adbaibi <adbaibi.ismail@gmail.com>
2020-07-21 02:11:26 +0200

    ok

--- genome/calcule/repetition.py
+++ genome/calcule/repetition.py
@@ -5,26 +5,57 @@ class Repetition():
 
     def __init__(self,chaine:str=""):
         self.chaine=chaine
-        self.combinaison=dict()
+        self.combinaison:dict=[]
         self.groupe="ACGT"
-        for i in range(len(self.groupe)):
-            self.combinaison[self.groupe[i]]=list()
-        print(self.combinaison)
-
-    def combi(self):
-        t=list()
-        tt=""
-        cpt=0
+        
+    def couper(self):
+        a=[]
+        tmp=""
+        for i in range(1,len(self.chaine)):
+            for ii in range(0,i):
+                tmp+=self.chaine[ii]
+                
+            a.append(tmp)
+            tmp=""
+        self.combinaison=a
     
-        for i in self.groupe:
-            pass
+    def affichage(self):
+        for i in self.combinaison:
+            print(i)
 
-a=Repetition()
-a.combi()
+    def affichage_N(self,N):
+        for i in range(N):
+            print(self.combinaison[i])
+
+    def nombre_repeter(self,n):
+        for i in range(0,n):
+            print(i,self.combinaison[i], self.chaine.count(self.combinaison[i]))
 
 
-a = ["A","C","G","T"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""a = ["A","C","G","T","A"]
+b=list()
 #print (list(itertools.combinations(a,2)))
 for i in range(len(a)):
-   print(list(itertools.permutations(a,i)))
- 
+   b=a[i]
+   for ii in range(len(a)):
+       print(b+a[ii])
+"""
+
+
+
+
+
