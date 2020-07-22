77d5e289376b9dc20218243937dc683266e1059d
adbaibi <adbaibi.ismail@gmail.com>
2020-07-23 01:46:49 +0200

    ok

---
+++ tp11/Constant.py
@@ -0,0 +1,6 @@
+from math import sqrt as race
+
+class Constant():
+    def __init__(self):
+        self.lor=(race(5)+1)/2
+
--- tp11/Decrypto.py
+++ tp11/Decrypto.py
@@ -1,9 +1,14 @@
+from Constant import Constant
 
+class Decrypto():
 
-class Derypto():
-
-    def __init__(self):
-        pass
+    def __init__(self, v:float):
+        self.v:float=v
+        self.lor=Constant().lor
 
     def test(self):
         pass
+
+
+d=Decrypto(2.3)
+d.test()
