efd5c0cf515215e3da3efe8dccb6a4611cf41e47
adbaibi <adbaibi.ismail@gmail.com>
2021-06-11 15:07:53 +0200

    espace ajouter

--- test13/main.py
+++ test13/main.py
@@ -3,3 +3,5 @@ p=Personne("Adbaibi","Camy",2,1,1980)
 print(p.nom)
 print(p.prenom)
 print(p.date_anniversaire)
+p.__nom="bo"
+print(p.nom)
--- test13/personne.py
+++ test13/personne.py
@@ -1,13 +1,15 @@
 from my_date import My_date
-class Personne:
 
-    __nom:str
-    __prenom:str
-    __date:My_date
-    def __init__(self, nom:str,prenom:str,jour:int,mois:int,anne:int):
-        self.__nom=nom
-        self.__prenom=prenom
-        self.__date=My_date(jour,mois,anne)
+
+class Personne:
+    __nom: str
+    __prenom: str
+    __date: My_date
+
+    def __init__(self, nom: str, prenom: str, jour: int, mois: int, anne: int):
+        self.__nom = nom
+        self.__prenom = prenom
+        self.__date = My_date(jour, mois, anne)
 
     @property
     def nom(self):
@@ -20,4 +22,3 @@ class Personne:
     @property
     def date_anniversaire(self):
         return self.__date.date
-
