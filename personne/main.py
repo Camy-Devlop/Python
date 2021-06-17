ba4f8ac4fbc9c6662e10eedee145f53764970477
adbaibi <adbaibi.ismail@gmail.com>
2021-06-17 23:26:15 +0200

    test

--- personne/main.py
+++ personne/main.py
@@ -3,7 +3,7 @@ from personne import Personne
 #nous avons inisialiser un constructeur Personne et on la stocker dans p
 # puis on va affichier le nom le prenom et la date
 p=Personne("Doudou","Camy",17,7,1980)
-
+#test
 print(p)
 
 print(p.combien_Jour_reste)
