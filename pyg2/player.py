582216040a2e87daef5a2cf40eeb1b8d7712c5b8
adbaibi ismaïl <63613252+Camy-Devlop@users.noreply.github.com>
2021-08-15 13:39:26 +0200

    nouveau code

--- pyg2/player.py
+++ pyg2/player.py
@@ -1,37 +1,55 @@
 import pygame
+import pytmx
+import pyscroll
+import os
+import arcade
 
 
-class Player(pygame.sprite.Sprite):
+from player import Player
 
-    def __init__(self, x, y):
-        super(Player, self).__init__()
-        self.sprite_sheet = pygame.image.load('player.png')
-        self.sprite_sheet.convert_alpha()
-        self.image = self.get_image(0, 0)
-        self.rect = self.image.get_rect()
-        self.position = [x, y]
-        self.images = {
-            'down': self.get_image(0, 0),
-            'left': self.get_image(0, 32),
-            'right': self.get_image(0, 64),
-            'up': self.get_image(0, 96)
-        }
-        self.speed = 1
 
-    def change_animation(self, name): self.image = self.images[name]
+class Game():
 
-    def move_right(self): self.position[0] += self.speed
+    def __init__(self):
+        self.screen = pygame.display.set_mode((800,600))
+        pygame.display.set_caption("nouveau jeux")
+        tmx_data = pytmx.util_pygame.load_pygame("carte1.tmx",pixelalpha=True)
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_data.visible_object_layers
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data,self.screen.get_size())
+        map_layer.zoom=2
+        self.fps=60
 
-    def move_left(self): self.position[0] -= self.speed
+        self.player=Player(30,40,tmx_data)
+        self.walls = []
+        for obj in tmx_data.objects:
+            print( obj.type)
+            if obj.type == "collision":
+                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
 
-    def move_up(self): self.position[1] -= self.speed
 
-    def move_down(self): self.position[1] += self.speed
+        self.groupe= pyscroll.PyscrollGroup(map_layer=map_layer,default_layer=4)
+        self.groupe.add(self.player)
+        #self.player = pygame.image.load("player.png")
 
     def update(self):
-        self.rect.topleft = self.position
+        self.groupe.update()
+        for sprite in self.groupe.sprites():
+            if sprite.feet.collidelist(self.walls)> -1:
+                self.player.move_back()
 
-    def get_image(self, x, y):
-        image = pygame.Surface([32, 32])
-        image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32,))
-        return image
+    def run(self):
+        running =True
+        clock = pygame.time.Clock()
+        while running:
+
+            self.player.save_location()
+            self.update()
+            self.groupe.center(self.player.rect)
+            self.groupe.draw(self.screen)
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+        self.clock.tick(self.fps)
+        pygame.quit()
