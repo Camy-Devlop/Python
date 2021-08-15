26a4698ece31e1c6b583b279530c61e9e517458a
adbaibi <adbaibi.ismail@gmail.com>
2021-08-15 18:15:37 +0200

    nouveau joueur

--- pyg/player.py
+++ pyg/player.py
@@ -7,17 +7,17 @@ class Player(pygame.sprite.Sprite):
     def __init__(self,x,y,tmx_data):
         super().__init__()
         self.tmx_data=tmx_data
-        self.sprite_sheet = pygame.image.load('player.png')
-        self.image=self.get_image(0,0)
+        self.sprite_sheet = pygame.image.load('player2.png')
+        self.image=self.get_image(0,64)
         #self.image.set_colorkey([0,0,0])
         self.rect = self.image.get_rect()
         self.feet=pygame.Rect(0,0,self.rect.width*0.5,12)
 
         self.images = {
-            'down': self.get_image(0, 0),
-            'left': self.get_image(0, 32),
-            'right': self.get_image(0, 64),
-            'up': self.get_image(0, 96)
+            'down': self.get_image(0, 64),
+            'left': self.get_image(0, 192),
+            'right': self.get_image(0, 128),
+            'up': self.get_image(0, 0)
         }
 
         self.speed = 1
@@ -43,8 +43,8 @@ class Player(pygame.sprite.Sprite):
 
 
     def get_image(self,x,y):
-        image=pygame.Surface([32,32])
-        image.blit(self.sprite_sheet,(0,0),(x,y,32,32))
+        image=pygame.Surface([32,64])
+        image.blit(self.sprite_sheet,(0,0),(x,y,32,64))
         return image
 
     def change_animation(self, name): self.image = self.images[name]
