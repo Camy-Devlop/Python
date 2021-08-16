72e6c6eb79e894b938371f0d434a9b8828691ec1
adbaibi <adbaibi.ismail@gmail.com>
2021-08-16 17:07:02 +0200

    nouveau joueur

--- pyg/game.py
+++ pyg/game.py
@@ -14,6 +14,7 @@ class Game():
         self.screen = pygame.display.set_mode((800,600))
         pygame.display.set_caption("nouveau jeux")
         tmx_data = pytmx.util_pygame.load_pygame("carte1.tmx",pixelalpha=True)
+
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_data.visible_object_layers
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data,self.screen.get_size())
