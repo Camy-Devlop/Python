5df5e4b426119d6c533087502555413803932639
adbaibi <adbaibi.ismail@gmail.com>
2021-08-18 01:02:20 +0200

    les entre et les sortie de la maisson est fini

--- pyg/game.py
+++ pyg/game.py
@@ -20,7 +20,7 @@ class Game():
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data,self.screen.get_size())
         map_layer.zoom=2
         self.fps=60
-        self.map="world"
+        self.map="house"
         self.player=Player(30,40,tmx_data)
         self.walls = []
         for obj in tmx_data.objects:
@@ -53,15 +53,22 @@ class Game():
                 self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
 
        #
+
+
         enter_house = tmx_data.get_object_by_name("exit_house")
         self.enter_house_rect = pygame.Rect(enter_house.x, enter_house.y, enter_house.width, enter_house.height)
         spawn_house_point = tmx_data.get_object_by_name("spawn_house")
+        print(spawn_house_point.x)
         self.player.position[0] = spawn_house_point.x
         self.player.position[1] = spawn_house_point.y - 20
+        self.groupe = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=8)
+        self.player.update()
+        self.groupe.add(self.player)
+        self.map = 'world'
 
     def switch_world(self):
 
-        tmx_data = pytmx.util_pygame.load_pygame("carte.tmx", pixelalpha=True)
+        tmx_data = pytmx.util_pygame.load_pygame("carte1.tmx", pixelalpha=True)
         map_data = pyscroll.data.TiledMapData(tmx_data)
 
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
@@ -74,23 +81,31 @@ class Game():
                 self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
 
 
+
        # self.player = pygame.image.load("player.png")
         enter_house = tmx_data.get_object_by_name("enter_house")
         self.enter_house_rect = pygame.Rect(enter_house.x, enter_house.y, enter_house.width, enter_house.height)
         spawn_house_point= tmx_data.get_object_by_name("enter_house_exit")
 
         self.player.position[0] = spawn_house_point.x
-        self.player.position[1] = spawn_house_point.y - 20
+        self.player.position[1] = spawn_house_point.y + 20
+        self.groupe = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=8)
+        self.player.update()
+        self.groupe.add(self.player)
+        self.map = 'house'
 
     def update(self):
         self.groupe.update()
 
-        if self.player.feet.colliderect(self.enter_house_rect):
-            self.switch_house()
-            self.map = 'house'
         if self.map == "house" and self.player.feet.colliderect(self.enter_house_rect):
+            self.switch_house()
+
+        if self.map == "world" and self.player.feet.colliderect(self.enter_house_rect):
+            print("la valeur de self.map fonction world", self.map)
             self.switch_world()
-            self.map = 'world'
+
+
+
 
         for sprite in self.groupe.sprites():
             if sprite.feet.collidelist(self.walls)> -1:
