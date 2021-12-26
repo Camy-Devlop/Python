4c0b3f0ecc82a29b771cc53e80c67c1e005b67c4
Camy-Devlop <adbaibi.ismail@gmail.com>
2021-12-26 03:48:37 +0100

    cration d'un bot trading

---
+++ BotTader/main.py
@@ -0,0 +1,27 @@
+
+
+# This is a sample Python script.
+
+# Press ⇧F10 to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+#pip install binance
+#pip install python-binance
+#pip install pandas
+#pip install ta
+#pip install aiohttp
+#pip install numpy
+import pandas as pd
+from binance.client import Client
+klinesT =Client().get_historical_klines("BTCUSDT",Client.KLINE_INTERVAL_1HOUR,
+                                        "01 January 2000")
+df = pd.DataFrame(klinesT,columns=['timestamp','open','high','low','close','volume','close_time','quote_av','trades','tb_base_av','tb_quote_av','ignore'])
+
+del df['ignore']
+del df['quote_av']
+del df['tb_base_av']
+del df['tb_quote_av']
+del df['trades']
+del df['close_time']
+
+print(df)
